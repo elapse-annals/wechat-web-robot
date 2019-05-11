51b145fb7ba44ec7ee1a2a72e8fda87a83c2fc1e
ben <benhuang1024@gmail.com>
2019-05-11 09:54:13 +0800

    更新基类版本

--- public/run.php
+++ public/run.php
@@ -6,6 +6,7 @@ $path = __DIR__ . '/tmp/';
 
 $options = [
     'path'      => $path,
+    'is_multi'  => true,
     /*
     * swoole 配置项（执行主动发消息命令必须要开启，且必须安装 swoole 插件）
     */
@@ -76,8 +77,15 @@ while (!checkPortBindable($options['swoole']['ip'], $options['swoole']['port']))
     $options['swoole']['port'] += 1;
 }
 if ($argv && isset($argv[1])) {
-    $argv[1] = $argv[1] . '_' . $options['swoole']['port'];
+    $getopts = getopt(null, ['session::']);
+    $session_key = $getopts['session'];
 }
+if (empty($session_key)) {
+    $session_key = 'vbot';
+}
+$options['console']['session_key'] = $session_key . '_' . $options['swoole']['port'];
+$options['console']['image_path'] = __DIR__ . '/images/' . $session_key . '_' . $options['swoole']['port'] . ".jpg";
+
 $vbot = new Hanson\Vbot\Foundation\Vbot($options);
 $vbot->messageHandler->setHandler(function ($message) {
     Hanson\Vbot\Message\Text::send($message['from']['UserName'], 'Hi, I\'m Vbot!');
